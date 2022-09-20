f49094c21b0e57eb611d5bee9328e177c369f445
Carlos24111 <112120124+Carlos24111@users.noreply.github.com>
2022-09-19 19:25:39 -0600

    Add files via upload

---
+++ Lab02/pagoporhora.py
@@ -0,0 +1,6 @@
+# Tendencia e Innovación en Tecnologia Agricola (TEA)
+
+HorasTrabajadas = input("Ingrese las horas trabajadas: ")
+valorHora = input("Ingrese el valor hora: ")
+pagoHora= int(HorasTrabajadas) * float(valorHora)
+print(pagoHora)
